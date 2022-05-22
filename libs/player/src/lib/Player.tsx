1cb86025816edf8b5c7f4e54cc139a3ba9d4e9bc
Philip <philip_angelin@hotmail.com>
2022-05-22 23:59:14 +0200

    feat: fullscreen

--- libs/player/src/lib/Player.tsx
+++ libs/player/src/lib/Player.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect } from "react";
 import "./player.scss";
 
 export interface PlayerProps {
@@ -16,6 +16,7 @@ export function Player({
 	track,
 	autoplay,
 }: PlayerProps) {
+	const wrapperEl = useRef<HTMLDivElement>(null);
 	const videoEl = useRef<HTMLVideoElement>(null);
 	const timelineEl = useRef<HTMLDivElement>(null);
 	const controlsEl = useRef<HTMLDivElement>(null);
@@ -24,6 +25,7 @@ export function Player({
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [isEnded, setIsEnded] = useState(false);
 	const [isScrubbing, setIsScrubbing] = useState(false);
+	const [isFullscreen, setIsFullscreen] = useState(false);
 
 	useEffect(() => {
 		if (!videoEl.current) {
@@ -89,6 +91,16 @@ export function Player({
 	}
 
 	useEffect(() => {
+		function fullscreenHandler() {
+			setIsFullscreen(Boolean(document.fullscreenElement));
+		}
+		document.addEventListener("fullscreenchange", fullscreenHandler);
+		return () => {
+			document.removeEventListener("fullscreenchange", fullscreenHandler);
+		};
+	}, []);
+
+	useEffect(() => {
 		function customContextMenu(e: MouseEvent) {
 			if (!videoEl.current || !controlsEl.current) {
 				return;
@@ -145,7 +157,7 @@ export function Player({
 	}
 
 	return (
-		<div className="AngelinPlayer" data-paused={!isPlaying}>
+		<div className="AngelinPlayer" data-paused={!isPlaying} ref={wrapperEl}>
 			{videoEl.current && (
 				<svg
 					className="AngelinPlayer__big-resume-icon"
@@ -156,7 +168,7 @@ export function Player({
 				</svg>
 			)}
 			<div className="AngelinPlayer__controls" ref={controlsEl}>
-				{videoEl.current && isLoaded && (
+				{videoEl.current && isLoaded && videoEl.current.duration > 0 && (
 					<div
 						className="AngelinPlayer__timeline"
 						ref={timelineEl}
@@ -174,43 +186,72 @@ export function Player({
 						</div>
 					</div>
 				)}
-				{isPlaying ? (
-					<button
-						className="AngelinPlayer__button"
-						disabled={!isLoaded}
-						onClick={pause}
-					>
-						<svg
-							className="AngelinPlayer__icon"
-							focusable="false"
-							viewBox="0 0 24 24"
+				<div className="AngelinPlayer__controls-buttons">
+					{isPlaying ? (
+						<button
+							className="AngelinPlayer__button"
+							disabled={!isLoaded}
+							onClick={pause}
 						>
-							<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
-						</svg>
-					</button>
-				) : (
-					<button
-						className="AngelinPlayer__button"
-						disabled={!isLoaded}
-						onClick={play}
-					>
-						{isEnded ? (
+							<svg
+								className="AngelinPlayer__icon"
+								focusable="false"
+								viewBox="0 0 24 24"
+							>
+								<path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"></path>
+							</svg>
+						</button>
+					) : (
+						<button
+							className="AngelinPlayer__button"
+							disabled={!isLoaded}
+							onClick={play}
+						>
+							{isEnded ? (
+								<svg
+									className="AngelinPlayer__icon"
+									viewBox="0 0 24 24"
+								>
+									<path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"></path>
+								</svg>
+							) : (
+								<svg
+									className="AngelinPlayer__icon"
+									viewBox="0 0 24 24"
+								>
+									<path d="M8 5v14l11-7z"></path>
+								</svg>
+							)}
+						</button>
+					)}
+					{isFullscreen ? (
+						<button
+							className="AngelinPlayer__button"
+							onClick={document.exitFullscreen}
+						>
 							<svg
 								className="AngelinPlayer__icon"
 								viewBox="0 0 24 24"
 							>
-								<path d="M12 5V1L7 6l5 5V7c3.31 0 6 2.69 6 6s-2.69 6-6 6-6-2.69-6-6H4c0 4.42 3.58 8 8 8s8-3.58 8-8-3.58-8-8-8z"></path>
+								<path d="M5 16h3v3h2v-5H5v2zm3-8H5v2h5V5H8v3zm6 11h2v-3h3v-2h-5v5zm2-11V5h-2v5h5V8h-3z"></path>
 							</svg>
-						) : (
+						</button>
+					) : (
+						<button
+							className="AngelinPlayer__button"
+							onClick={() =>
+								wrapperEl.current?.requestFullscreen()
+							}
+						>
 							<svg
 								className="AngelinPlayer__icon"
 								viewBox="0 0 24 24"
 							>
-								<path d="M8 5v14l11-7z"></path>
+								<path d="M7 14H5v5h5v-2H7v-3zm-2-4h2V7h3V5H5v5zm12 7h-3v2h5v-5h-2v3zM14 5v2h3v3h2V5h-5z"></path>
 							</svg>
-						)}
-					</button>
-				)}
+						</button>
+					)}
+				</div>
 			</div>
 			<video
 				onLoadedData={() => setIsLoaded(true)}
